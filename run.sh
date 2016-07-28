05050bc6153b07120e2d692acc0a37414ab83b7c
talm <talmachani@gmail.com>
2016-07-28 21:50:27 +0300

    improved run file

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env bash
-
-# Assumes the config file is created and named config.json
-# Starts PokemonGo-Bot
-eval "python pokecli.py --config ./configs/config.json"
+#!/bin/bash
+fname=`basename $1`
+while true
+do
+        python pokecli.py -cf /home/ubuntu/$1 >> logs/log-$fname.log 2>> /logs/error-$fname.log;
+        echo ">Bot crashed. Restarting...";
+        sleep 5;
+done
