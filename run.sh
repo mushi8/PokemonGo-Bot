128be0cf50e9b251b109a12d826f41c8f25831c8
talm <talmachani@gmail.com>
2016-07-29 08:56:23 +0300

    run fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 fname=`basename $1`
 while true
 do
-        python pokecli.py -cf /home/ubuntu/$1 >> logs/log-$fname.log 2>> /logs/error-$fname.log;
+        python pokecli.py -cf $1 >> logs/log-$fname.log 2>> logs/error-$fname.log;
         echo ">Bot crashed. Restarting...";
         sleep 5;
 done
